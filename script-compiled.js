f67d6653d7e0102610038b4108e1aafe68150657
Guðmundur Guðbjarnason <Gummi@Gumundurs-MacBook-Pro-3.local>
2017-11-23 02:52:10 +0000

    js fyrir player

--- script-compiled.js
+++ script-compiled.js
@@ -93,6 +93,44 @@ document.addEventListener('DOMContentLoaded', function () {
   var index = new Index();
   index.load();
 });
-"use strict";
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Video = function () {
+  function Video() {
+    _classCallCheck(this, Video);
+
+    this.container = document.querySelector('.categories');
+  }
+
+  _createClass(Video, [{
+    key: 'load',
+    value: function load() {
+      var _this = this;
+
+      var request = new Request('videos.json', { method: 'GET' });
+      fetch(request).then(function (response) {
+        if (response.status === 200) {
+          return response.json();
+        }
+        throw new Error('Something went wrong on api server!');
+      }).then(function (data) {
+        _this.parseData(data);
+      }).catch(function (error) {
+        console.error(error); // eslint-disable-line
+      });
+    }
+  }]);
+
+  return Video;
+}();
+
+document.addEventListener('DOMContentLoaded', function () {
+  var video = new Video();
+  Video.load();
+});
 
 //# sourceMappingURL=script-compiled.js.map
